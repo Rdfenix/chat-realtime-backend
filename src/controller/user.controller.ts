5aff4eec6da2a29a0eef555adcb0abde67a97fd3
rdfenix <rhoodsk8erboy@gmail.com>
2023-10-26 12:18:49 -0300

    create: updating response object

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -3,7 +3,7 @@ import { Request, Response } from "express";
 
 const createUser = async (req: Request, res: Response) => {
   if (!req.body.name || !req.body.username || !req.body.password) {
-    res.status(400).json({ message: "Content cant be empty" });
+    res.status(400).json({ user: "", message: "Content cant be empty" });
   }
 
   const { name, password, username } = req.body;
@@ -16,9 +16,10 @@ const createUser = async (req: Request, res: Response) => {
 
   try {
     const result = await user.save();
-    res.status(200).json(result);
+    res.status(200).json({ user: result, message: "Created" });
   } catch (error: any) {
     res.status(500).json({
+      user: "",
       message: error.message || "Some error occurred while creating user",
     });
   }
@@ -26,18 +27,18 @@ const createUser = async (req: Request, res: Response) => {
 
 const findUser = async (req: Request, res: Response) => {
   try {
-    const username = req.body.username;
-    const user = await UserModel.findOne({ username });
-    res.status(200).json(user);
+    const idUser = req.query.id;
+    const user = await UserModel.findById(idUser);
+    res.status(200).json({ user, message: "Found" });
   } catch (error: any) {
-    res.status(404).json({ message: error.message });
+    res.status(404).json({ user: "", message: error.message });
   }
 };
 
 const loginUser = async (req: Request, res: Response) => {
   try {
     if (!req.body.username || !req.body.password) {
-      res.status(400).json({ message: "Content cant be empty" });
+      res.status(400).json({ user: "", message: "Content cant be empty" });
     }
 
     const { username, password } = req.body;
@@ -46,10 +47,10 @@ const loginUser = async (req: Request, res: Response) => {
     if (user && user.password === password) {
       res.status(200).json({ user, message: "signIn" });
     } else {
-      res.status(401).json({ message: "Unauthorized" });
+      res.status(401).json({ user: "", message: "Unauthorized" });
     }
   } catch (error: any) {
-    res.status(404).json({ message: error.message });
+    res.status(404).json({ user: "", message: error.message });
   }
 };
 
